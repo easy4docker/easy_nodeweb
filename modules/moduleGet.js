4282950dc4a6d470509cac0bca5c5dd4fbf40fa0
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-02-28 07:04:24 -0800

    Update moduleGet.js

--- modules/moduleGet.js
+++ modules/moduleGet.js
@@ -9,7 +9,7 @@
 			const dirn = '/var/_shareFolder';
 			fs.readdir(dirn, (err, files) => {
 				if (files.length) {
-					fs.readFile(dirn + '/' + files[0], 'utf8', (err, content) => {
+					fs.readdir(dirn + '/' + files[0], (err, content) => {
 						cbk((err)?err.message : content);
 					});
 				} else {
