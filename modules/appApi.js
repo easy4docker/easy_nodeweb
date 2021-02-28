1f64a0bfc813eebb22c334a35802931216c2af02
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-02-27 21:01:08 -0800

    Update appApi.js

--- modules/appApi.js
+++ modules/appApi.js
@@ -4,7 +4,10 @@
 		    me = this;
 		
 		me.call = function(rest, incomeData, callback) {
-			callback({rest: rest, incomeData: incomeData, dir: __dirname, env : env });
+			let REST_Module = pkg.require(__dirname + '/moduleGet.js.js');
+			let getm = new REST_Module(env, pkg);
+			getm.run(incomeData.path, callback);
+			// callback({rest: rest, incomeData: incomeData, dir: __dirname, env : env });
 		};	
 	};
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
