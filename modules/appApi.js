bb5f0ac837f79a15da79b75b13ec851da901cf8f
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-02-27 20:43:30 -0800

    Update appApi.js

--- modules/appApi.js
+++ modules/appApi.js
@@ -1,10 +1,10 @@
 (function () { 
 	var obj =  function (env, pkg) {
-        var fs = require('fs'),
-            me = this;
+		var fs = require('fs'),
+		    me = this;
 		
-		this.call = function(rest, incomeData, callback) {
-            callback({mark : 'A', incomeData: incomeData, dir: __dirname, env : env });
+		me.call = function(rest, incomeData, callback) {
+			callback({incomeData: incomeData, dir: __dirname, env : env });
 		};	
 	};
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
