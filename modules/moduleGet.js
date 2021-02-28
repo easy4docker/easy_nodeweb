ee3ba94d65cb8c347cd8a04face420d5205d9fb3
easy4docker <76085700+easy4docker@users.noreply.github.com>
2021-02-27 20:53:14 -0800

    Create moduleGet.js

---
+++ modules/moduleGet.js
@@ -0,0 +1,15 @@
+(function () { //
+	var obj =  function (env, pkg, req, res) {
+		let fs = require('fs');
+		let me = this;
+    me.run = (path) {
+    }
+    me.showShareFolder = () {
+      
+    }
+	};
+	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+		module.exports = obj;
+	} 
+
+})();
